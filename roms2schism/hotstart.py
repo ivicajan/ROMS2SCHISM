d1251787c2484d87e7fecd2f172d20be8372da3b
Adrian Croucher <a.croucher@auckland.ac.nz>
2023-06-07 14:59:25 +1200

    hotstart cumsum_eta is an array

--- roms2schism/hotstart.py
+++ roms2schism/hotstart.py
@@ -39,10 +39,6 @@ def save_hotstart_nc(outfile, eta2_data, temp_data, salt_data,
     dst['nsteps_from_cold'][:] = 0
     nfc.long_name = 'number of steps from cold start'
 
-    cse = dst.createVariable('cumsum_eta', 'f8', ('one_new'))
-    dst['cumsum_eta'][:] = 0
-    cse.long_name = 'cumsum eta'
-
     # dry nodes, sides, elements:
 
     idry = dst.createVariable('idry', 'i4', ('node'))
@@ -67,6 +63,10 @@ def save_hotstart_nc(outfile, eta2_data, temp_data, salt_data,
     eta2 = dst.createVariable('eta2', 'f8', ('node'))
     dst['eta2'][:] = eta2_data
     eta2.long_name = "elevation at nodes at current timestep"
+
+    cse = dst.createVariable('cumsum_eta', 'f8', ('node'))
+    dst['cumsum_eta'][:] = 0
+    cse.long_name = 'cumsum eta'
 
     su2 = dst.createVariable('su2', 'f8', ('side', 'nVert'))
     dst['su2'][:,:] = su2_data
