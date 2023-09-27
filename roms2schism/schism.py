404ba059e12c6e02862832750a3d42a78a9034d6
Ivica Janekovic <ivica.jan@gmail.com>
2023-09-27 14:22:11 +0800

    cleaned scshism.py

--- roms2schism/schism.py
+++ roms2schism/schism.py
@@ -15,45 +15,11 @@ import os
 from itertools import islice
 import numpy as np
 from pyschism.mesh import Hgrid
-#from pyschism.mesh.vgrid import Vgrid
 from roms2schism.geometry import transform_ll_to_cpp, bbox
-
-class gr3(object):
-    """Class for gr3 grid"""
-
-    def __init__(self, filename):
-
-        with open(filename,'r') as fid:
-            # grid  name
-            self.name = fid.readline().strip()
-            # number of elements and nodes
-            tmp = fid.readline().split()
-            self.ne = int(tmp[0])
-            self.nn = int(tmp[1])
-            # first load nodes and values
-            # not using nn
-            tmp = list(islice(fid, self.nn))
-            node_id, self.x, self.y, self.z = np.loadtxt(tmp,
-                                                         dtype = {'names':('n','x','y','z'),
-                                                                  'formats':('i4','f8','f8','f8')},
-                                                         usecols = (0,1,2,3),
-                                                         unpack=True)
-            del node_id
-            # elements
-            tmp = list(islice(fid, self.ne))
-            tmp_e = np.loadtxt(tmp, dtype='i4')
-            self.e = tmp_e[:,2:] - 1
 
 class schism_vgrid:
     def __init__(self):
         pass
-
-    @property
-    def INFO(self):
-        return get_INFO(self)
-    @property
-    def VINFO(self):
-        return get_INFO(self)
 
     def read_vgrid(self,fname):
         #read schism vgrid
@@ -219,14 +185,6 @@ class schism_grid(object):
         hgrid = Hgrid.open(hgrid_filename,  crs = 'EPSG:4326')
         # get schism depths
         vgrid_filename = os.path.join(schism_grid_dir, schism_vgrid_file)
-        #vd = Vgrid.open(vgrid_filename)
-        #sigma = vd.sigma              # sigma values for vertical grid
-        #depth = hgrid.values          # this is grid bathymery
-        #zcor = depth[:,None] * sigma  # this is 2D array with layer depths at [nodes, layers]
-        #nvrt = zcor.shape[1]          # number of SCHISM layers
-
-        # alternative way using pylib instead Vgrid
-        #gd=read(schism_grid_file)
         vd=read_schism_vgrid(schism_vgrid_file)
         zcor=vd.compute_zcor(-hgrid.values)
         nvrt = zcor.shape[1] 
