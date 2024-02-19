d04766f60a58fe6a040170506ac444235f012229
Ivica Janekovic <ivica.jan@gmail.com>
2024-02-19 08:54:54 +0800

    Cleaned a bit

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as f:
 
 setuptools.setup(
     name='roms2schism',
-    version='0.8.0',
+    version='0.9.0',
     description='A Python package for creating SCHISM boundary condition forcing and hotstart files from ROMS output',
     long_description=long_description,
     long_description_content_type="text/markdown",
@@ -16,6 +16,5 @@ setuptools.setup(
         "Programming Language :: Python :: 3",
         "Operating System :: OS Independent"],
     python_requires='>=3.6',
-    install_requires=['progressbar2', 'netCDF4', 'numpy', 'scipy',
-                      'pyschism', 'matplotlib']
+    install_requires=['progressbar2', 'netCDF4', 'numpy', 'scipy', 'matplotlib']
 )
