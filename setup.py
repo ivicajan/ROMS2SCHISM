58932b65306f0f539a5de97e0059ba155619c4e6
Adrian Croucher <a.croucher@auckland.ac.nz>
2024-02-27 15:43:08 +1300

    Update to pyproject.toml for packaging

--- setup.py
+++ setup.py
@@ -1,20 +1,3 @@
-import setuptools
-
-with open("README.md", "r") as f:
-    long_description = f.read()
-
-setuptools.setup(
-    name='roms2schism',
-    version='0.9.0',
-    description='A Python package for creating SCHISM boundary condition forcing and hotstart files from ROMS output',
-    long_description=long_description,
-    long_description_content_type="text/markdown",
-    url='http://gitlab.com/acroucher/roms2schism',
-    author='Ivica Janekovic, Adrian Croucher',
-    packages=setuptools.find_packages(),
-    classifiers=[
-        "Programming Language :: Python :: 3",
-        "Operating System :: OS Independent"],
-    python_requires='>=3.6',
-    install_requires=['progressbar2', 'netCDF4', 'numpy', 'scipy', 'matplotlib']
-)
+from setuptools import setup
+# for backwards compatibility only
+setup()
